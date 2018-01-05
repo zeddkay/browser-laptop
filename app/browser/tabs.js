6628b9d87d65b42ee56fb86b9448740601e11331
Pete Miller <miller.pete+commit@gmail.com>
2018-01-05 16:13:13 +1300

    Introduce linux mousemove event forwarding for outside-window tab dragging

--- app/browser/tabs.js
+++ app/browser/tabs.js
@@ -634,7 +634,7 @@ const api = {
         // forget last active trail in window tab
         // is detaching from
         const oldTab = getTabValue(oldTabId)
-        const detachedFromWindowId = oldTab.get('windowId')
+        const detachedFromWindowId = oldTab ? oldTab.get('windowId') : null
         if (detachedFromWindowId != null) {
           activeTabHistory.clearTabFromWindow(detachedFromWindowId, oldTabId)
         }
