485f19ae547fa73541fcb38a9f635a3ddf0d855e
Pete Miller <miller.pete@gmail.com>
2018-02-26 22:19:30 -0800

    do not tell windowState to close frame when WebContents is disposed

--- app/renderer/components/frame/frame.js
+++ app/renderer/components/frame/frame.js
@@ -475,7 +475,7 @@ class Frame extends React.Component {
     }, { passive: true })
 
     this.tabEventEmitter.addEventListener('will-destroy', (e) => {
-      this.onCloseFrame()
+//      this.onCloseFrame()
     }, { passive: true })
     this.tabEventEmitter.addEventListener('page-favicon-updated', (e) => {
       console.log('page-fav-upd')
