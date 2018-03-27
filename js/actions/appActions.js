89750ab33ba77761d849551055d3da5352637934
petemill <miller.pete+commit@gmail.com>
2018-03-27 00:00:08 -0700

    do not display tab until tab-inserted-at has fired

--- js/actions/appActions.js
+++ js/actions/appActions.js
@@ -1976,6 +1976,17 @@ const appActions = {
       duration,
       revisited
     })
+  },
+
+  tabInsertedToTabStrip: function(windowId, tabId, index) {
+    dispatch({
+      actionType: appConstants.APP_TAB_INSERTED_TO_TAB_STRIP,
+      queryInfo: {
+        windowId
+      },
+      tabId,
+      index
+    })
   }
 }
 
